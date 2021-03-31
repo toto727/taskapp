c46e0dfad4c2a2ccb3b2f61cfcb8534aeaf6eb92
toto727 <80440486+toto727@users.noreply.github.com>
2021-03-31 21:12:24 +0900

    課題修正

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -6,17 +6,27 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // ユーザに通知の許可を求める 
+                let center = UNUserNotificationCenter.current()
+                center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                    // Enable or disable features based on authorization
+                }
         // Override point for customization after application launch.
+        center.delegate = self
         return true
     }
-
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド 
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            completionHandler([.banner, .list, .sound])
+        }
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
